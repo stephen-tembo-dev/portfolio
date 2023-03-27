55fe11aca3a5097f87b04bbba04cac77d80402e7
stephen tembo <stephen187dev@gmail.com>
2023-03-27 11:54:42 +0200

    initial commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+
+    $(function(){
+  
+      M.AutoInit();
+  
+      $('.slider').slider();
+      
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
